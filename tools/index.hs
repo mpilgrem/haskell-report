e04fe3dddf10cb0a855b1baaa789bdf85a2f6287
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2026-08-01 20:27:03 +0100

    Formatting: index.hs

--- tools/index.hs
+++ tools/index.hs
@@ -1,8 +1,6 @@
-
--- This generates the prelude index to the report by hyper-linking all
--- of the names in a hand-written document.  This is probably too
--- obscure to bother explaining.  Hardwires for the Haskell report at the
--- moment.
+-- This generates the prelude index to the report by hyper-linking all of the
+-- names in a hand-written document. This is probably too obscure to bother
+-- explaining. Hardwires for the Haskell report at the moment.
 
 module Main where
 
@@ -12,102 +10,107 @@ import Data.Maybe ( fromMaybe )
 import System.IO.Error ( catchIOError )
 
 main :: IO ()
-main = do refs <- readRefFile "reportrefs"
-          doFiles refs ["prelude-index"]
+main = do
+  refs <- readRefFile "reportrefs"
+  doFiles refs ["prelude-index"]
 
 doFiles :: Refs -> [FilePath] -> IO ()
-doFiles r files = do mapM_ (doFile r) files
-                     putStrLn "Done."
+doFiles r files = do
+  mapM_ (doFile r) files
+  putStrLn "Done."
 
 doFile :: Refs -> FilePath -> IO ()
 doFile r f = catchIOError
-               (do putStrLn ("Reading " ++ f ++ ".idx")
-                   ls <- readFile (f ++ ".idx")
-                   let output = expandAllRefs r (lines ls)
-                   writeFile ("haskell-report-html/" ++ f ++ ".html")
-                             (unlines output))
-               (\err -> putStrLn ("Error: " ++ show err))
+  ( do putStrLn ("Reading " ++ f ++ ".idx")
+       ls <- readFile (f ++ ".idx")
+       let output = expandAllRefs r (lines ls)
+       writeFile ("haskell-report-html/" ++ f ++ ".html") (unlines output)
+  )
+  (\err -> putStrLn ("Error: " ++ show err))
 
 -- This sets up the parts of the state that need to be reset at the start of
 -- each file.
 
-type Refs = [(String,String)]
+type Refs = [(String, String)]
 
 expandAllRefs :: Refs -> [String] -> [String]
 expandAllRefs r = expandAll1 r False
 
 expandAll1 :: Refs -> Bool -> [String] -> [String]
-expandAll1 _ _     [] = []
-expandAll1 r table (l:ls) | l == "#table" = expandAll1 r True ls
-                          | l == "#endtable" = expandAll1 r False ls
-                          | table = ("<tr><td><tt>" ++ nbspaces (expandRefs r l)
-                                     ++ "</tt></td></tr>") : rest
-                          | otherwise = expandRefs r l : rest
- where rest = expandAll1 r table ls
+expandAll1 _ _ [] = []
+expandAll1 r table (l : ls)
+  | l == "#table" = expandAll1 r True ls
+  | l == "#endtable" = expandAll1 r False ls
+  | table =   ("<tr><td><tt>" ++ nbspaces (expandRefs r l) ++ "</tt></td></tr>")
+            : rest
+  | otherwise = expandRefs r l : rest
+ where
+  rest = expandAll1 r table ls
 
 expandRefs :: Refs -> String -> String
 expandRefs _ "" = ""
-expandRefs r ('#':l) = expandRef r "" l
-expandRefs r (c:cs) = c : expandRefs r cs
+expandRefs r ('#' : l) = expandRef r "" l
+expandRefs r (c : cs) = c : expandRefs r cs
 
 expandRef :: Refs -> String -> String -> String
-expandRef r txt ('V':l) = expandVar r txt (parseRef l)
-expandRef r txt ('I':l) = expandInstance r txt (parseRef l)
-expandRef r txt ('T':l) = expandTycon r txt (parseRef l)
-expandRef r txt ('L':l) = expandLink r txt (parseRef l)
-expandRef r txt ('S':l) = expandSect r txt (parseRef l)
-expandRef r _   ('&':l) = "</tt></td><td><tt>" ++ expandRefs r l
-expandRef r _   ('#':l) = "#" ++ expandRefs r l
-expandRef r _   ('.':l) = expandRefs r l
+expandRef r txt ('V' : l) = expandVar r txt (parseRef l)
+expandRef r txt ('I' : l) = expandInstance r txt (parseRef l)
+expandRef r txt ('T' : l) = expandTycon r txt (parseRef l)
+expandRef r txt ('L' : l) = expandLink r txt (parseRef l)
+expandRef r txt ('S' : l) = expandSect r txt (parseRef l)
+expandRef r _   ('&' : l) = "</tt></td><td><tt>" ++ expandRefs r l
+expandRef r _   ('#' : l) = "#" ++ expandRefs r l
+expandRef r _   ('.' : l) = expandRefs r l
 expandRef _ _   l = error ("Bad ref:" ++ l ++ "\n")
 
 parseRef :: String -> (String, String)
 parseRef = break (\c -> isSpace c || c == '#')
 
 expandVar :: Refs -> String -> (String, String) -> String
-expandVar r txt (v,rest) = let n = mangleVar v
-                               f = lookup n r in
-                             case f of
-                               Nothing -> trySig r txt v rest n
-                               _ -> anchor v f n txt ++ expandRefs r rest
+expandVar r txt (v, rest) =
+  let n = mangleVar v
+      f = lookup n r
+  in  case f of
+        Nothing -> trySig r txt v rest n
+        _ -> anchor v f n txt ++ expandRefs r rest
 
 expandTycon :: Refs -> String -> (String, String) -> String
-expandTycon r txt (t,rest) = let n = mangleTycon t
-                                 f = lookup n r in
-                             anchor t f n txt ++ expandRefs r rest
+expandTycon r txt (t, rest) =
+  let n = mangleTycon t
+      f = lookup n r
+  in  anchor t f n txt ++ expandRefs r rest
 
 expandInstance :: Refs -> String -> (String, String) -> String
-expandInstance r txt (c,'#':rest ) = let (t,rest') = parseRef rest
-                                         n = mangleInstance c t
-                                         f = lookup n r in
-                                       anchor c f n txt ++ expandRefs r rest'
-expandInstance _ _   (_,l) = error ("bad instance " ++ l ++ "\n")
+expandInstance r txt (c, '#' : rest ) =
+  let (t, rest') = parseRef rest
+      n = mangleInstance c t
+      f = lookup n r
+  in  anchor c f n txt ++ expandRefs r rest'
+expandInstance _ _ (_, l) = error ("bad instance " ++ l ++ "\n")
 
 expandSect :: Refs -> String -> (String, String) -> String
-expandSect r txt (s,rest) = let n = mangleSect s
-                                f = lookup n r in
-                              "(see " ++ anchor s f n txt ++ ")" ++
-                               expandRefs r rest
+expandSect r txt (s, rest) =
+  let n = mangleSect s
+      f = lookup n r
+  in  "(see " ++ anchor s f n txt ++ ")" ++ expandRefs r rest
 
 expandLink :: Refs -> String -> (String, String) -> String
-expandLink r _ (t,'#':l') = expandRef r t l'
-expandLink _ _ (l,l') = error ("Bad link: " ++ l ++ l' ++ "\n")
+expandLink r _ (t, '#' : l') = expandRef r t l'
+expandLink _ _ (l, l') = error ("Bad link: " ++ l ++ l' ++ "\n")
 
 trySig :: Refs -> String -> String -> String -> String -> String
 trySig r txt v rest _n =
-   let c = parseClass rest
-       n = mangleTycon c
-       f = lookup n r in
-     anchor v f n txt ++ expandRefs r rest
+  let c = parseClass rest
+      n = mangleTycon c
+      f = lookup n r
+  in  anchor v f n txt ++ expandRefs r rest
 
 anchor :: String -> Maybe String -> String -> String -> String
-anchor str mfile tag txt =
-         case mfile of
-           Just f -> "<a href=\"" ++ f ++ ".html#" ++ tag ++
-                      "\">" ++ t ++ "</a>"
-           Nothing -> "Bad tag:" ++ tag ++ " " ++ t
-     where
-       t = htmlS $ if txt == "" then str else txt
+anchor str mfile tag txt = case mfile of
+  Just f -> "<a href=\"" ++ f ++ ".html#" ++ tag ++ "\">" ++ t ++ "</a>"
+  Nothing -> "Bad tag:" ++ tag ++ " " ++ t
+ where
+  t = htmlS $ if txt == "" then str else txt
 
 mangleVar :: String -> String
 mangleVar n = "$v" ++ mangleName (filter (`notElem` "()") n)
@@ -122,52 +125,55 @@ mangleSect :: String -> String
 mangleSect s = "sect" ++ s
 
 mangleName :: String -> String
-mangleName = concatMap
-                   (\c -> case c of '(' -> "$P"
-                                    ')' -> "$C"
-                                    '-' -> "$D"
-                                    '[' -> "$B"
-                                    ']' -> "$c"
-                                    ',' -> "$x"
-                                    '#' -> "$p"
-                                    '$' -> "$D"
-                                    '|' -> "$b"
-                                    '!' -> "$E"
-                                    '&' -> "$A"
-                                    '^' -> "$U"
-                                    '>' -> "$G"
-                                    '<' -> "$L"
-                                    '=' -> "$Q"
-                                    _   -> [c])
+mangleName = concatMap $ \c -> case c of
+  '(' -> "$P"
+  ')' -> "$C"
+  '-' -> "$D"
+  '[' -> "$B"
+  ']' -> "$c"
+  ',' -> "$x"
+  '#' -> "$p"
+  '$' -> "$D"
+  '|' -> "$b"
+  '!' -> "$E"
+  '&' -> "$A"
+  '^' -> "$U"
+  '>' -> "$G"
+  '<' -> "$L"
+  '=' -> "$Q"
+  _   -> [c]
 
 mangleType :: String -> String
-mangleType t = mangleName (case t of
-                              "(IO"    -> "IO"
-                              "(a->b)" -> "->"
-                              "[a]"    -> "[]"
-                              x        -> x)
-
-
+mangleType t = mangleName $ case t of
+  "(IO"    -> "IO"
+  "(a->b)" -> "->"
+  "[a]"    -> "[]"
+  x        -> x
 
 readRefFile :: String -> IO [(String, String)]
 readRefFile f = catchIOError
-                      (do l <- readFile f
-                          return (map parseKV (lines l)))
-                      (\e -> do putStrLn ("Can't read ref file: " ++ f)
-                                print e
-                                return [])
+  ( do l <- readFile f
+       return (map parseKV (lines l))
+  )
+  ( \e -> do
+      putStrLn ("Can't read ref file: " ++ f)
+      print e
+      return []
+  )
 
 parseKV :: String -> (String, String)
-parseKV l = let (k,l1) = span (/= '=') l
-                val    = case l1 of
-                           ('=':v) -> trim v
-                           _       -> ""
-              in (trimr k,val)
+parseKV l =
+  let (k, l1) = span (/= '=') l
+      val = case l1 of
+        ('=' : v) -> trim v
+        _ -> ""
+  in  (trimr k, val)
 
 parseClass :: String -> String
-parseClass s = let s1 = (skip "(" . skip "::") s
-                   (c,_) = span isAlpha (trim s1) in
-                 c
+parseClass s =
+  let s1 = (skip "(" . skip "::") s
+      (c, _) = span isAlpha (trim s1)
+  in  c
 
 trim :: String -> String
 trim = dropWhile isSpace
@@ -190,6 +196,7 @@ htmlS = concatMap htmlEncode
 nbspaces :: String -> String
 nbspaces "" = ""
 nbspaces (' ' : cs) = "&nbsp;" ++ nbspaces cs
-nbspaces ('<':cs) = ['<'] ++ c ++ nbspaces r where
-                        (c,r) = span (/= '>') cs
-nbspaces (c:cs) = c:nbspaces cs
+nbspaces ('<' : cs) = ['<'] ++ c ++ nbspaces r
+ where
+  (c, r) = span (/= '>') cs
+nbspaces (c : cs) = c : nbspaces cs
