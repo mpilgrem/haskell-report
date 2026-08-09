9711bf2bc87a1ddd972a03dd32b6fa09aab83ad4
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2026-08-09 21:18:18 +0100

    Update the Prelude

--- report/PreludeList.hs
+++ report/PreludeList.hs
@@ -1,14 +1,13 @@
 -- Standard list functions
 
 module PreludeList (
-    map, (++), filter, concat, concatMap, 
-    head, last, tail, init, null, length, (!!), 
-    foldl, foldl1, scanl, scanl1, foldr, foldr1, scanr, scanr1,
+    map, (++), filter,
+    head, last, tail, init, null, length, (!!),
+    scanl, scanl1, scanr, scanr1,
     iterate, repeat, replicate, cycle,
     take, drop, splitAt, takeWhile, dropWhile, span, break,
-    lines, words, unlines, unwords, reverse, and, or,
-    any, all, elem, notElem, lookup,
-    sum, product, maximum, minimum, 
+    lines, words, unlines, unwords, reverse,
+    lookup,
     zip, zip3, zipWith, zipWith3, unzip, unzip3)
   where
 
@@ -31,12 +30,6 @@ filter :: (a -> Bool) -> [a] -> [a]
 filter p []                 = []
 filter p (x:xs) | p x       = x : filter p xs
                 | otherwise = filter p xs
-
-concat :: [[a]] -> [a]
-concat xss = foldr (++) [] xss
-
-concatMap :: (a -> [b]) -> [a] -> [b]
-concatMap f = concat . map f
 
 -- head and tail extract the first element and remaining elements,
 -- respectively, of a list, which must be non-empty.  last and init
@@ -77,25 +70,12 @@ xs     !! n | n < 0 =  error "Prelude.!!: negative index"
 (x:_)  !! 0         =  x
 (_:xs) !! n         =  xs !! (n-1)
 
--- foldl, applied to a binary operator, a starting value (typically the
--- left-identity of the operator), and a list, reduces the list using
--- the binary operator, from left to right:
---  foldl f z [x1, x2, ..., xn] == (...((z `f` x1) `f` x2) `f`...) `f` xn
--- foldl1 is a variant that has no starting value argument, and  thus must
--- be applied to non-empty lists.  scanl is similar to foldl, but returns
--- a list of successive reduced values from the left:
+-- scanl is similar to foldl, but returns a list of successive reduced values
+-- from the left:
 --      scanl f z [x1, x2, ...] == [z, z `f` x1, (z `f` x1) `f` x2, ...]
 -- Note that  last (scanl f z xs) == foldl f z xs.
 -- scanl1 is similar, again without the starting element:
 --      scanl1 f [x1, x2, ...] == [x1, x1 `f` x2, ...]
-
-foldl            :: (a -> b -> a) -> a -> [b] -> a
-foldl f z []     =  z
-foldl f z (x:xs) =  foldl f (f z x) xs
-
-foldl1           :: (a -> a -> a) -> [a] -> a
-foldl1 f (x:xs)  =  foldl f x xs
-foldl1 _ []      =  error "Prelude.foldl1: empty list"
 
 scanl            :: (a -> b -> a) -> a -> [b] -> [a]
 scanl f q xs     =  q : (case xs of
@@ -109,25 +89,16 @@ scanl1 _ []      =  []
 -- foldr, foldr1, scanr, and scanr1 are the right-to-left duals of the
 -- above functions.
 
-foldr            :: (a -> b -> b) -> b -> [a] -> b
-foldr f z []     =  z
-foldr f z (x:xs) =  f x (foldr f z xs)
-
-foldr1           :: (a -> a -> a) -> [a] -> a
-foldr1 f [x]     =  x
-foldr1 f (x:xs)  =  f x (foldr1 f xs)
-foldr1 _ []      =  error "Prelude.foldr1: empty list"
-
 scanr             :: (a -> b -> b) -> b -> [a] -> [b]
 scanr f q0 []     =  [q0]
 scanr f q0 (x:xs) =  f x q : qs
-                     where qs@(q:_) = scanr f q0 xs 
+                     where qs@(q:_) = scanr f q0 xs
 
 scanr1          :: (a -> a -> a) -> [a] -> [a]
 scanr1 f []     =  []
 scanr1 f [x]    =  [x]
 scanr1 f (x:xs) =  f x q : qs
-                   where qs@(q:_) = scanr1 f xs 
+                   where qs@(q:_) = scanr1 f xs
 
 -- iterate f x returns an infinite list of repeated applications of f to x:
 -- iterate f x == [x, f x, f (f x), ...]
@@ -170,12 +141,12 @@ splitAt n xs             =  (take n xs, drop n xs)
 
 -- takeWhile, applied to a predicate p and a list xs, returns the longest
 -- prefix (possibly empty) of xs of elements that satisfy p.  dropWhile p xs
--- returns the remaining suffix.  span p xs is equivalent to 
+-- returns the remaining suffix.  span p xs is equivalent to
 -- (takeWhile p xs, dropWhile p xs), while break p uses the negation of p.
 
 takeWhile               :: (a -> Bool) -> [a] -> [a]
 takeWhile p []          =  []
-takeWhile p (x:xs) 
+takeWhile p (x:xs)
             | p x       =  x : takeWhile p xs
             | otherwise =  []
 
@@ -187,8 +158,8 @@ dropWhile p xs@(x:xs')
 
 span, break             :: (a -> Bool) -> [a] -> ([a],[a])
 span p []            = ([],[])
-span p xs@(x:xs') 
-            | p x       =  (x:ys,zs) 
+span p xs@(x:xs')
+            | p x       =  (x:ys,zs)
             | otherwise =  ([],xs)
                            where (ys,zs) = span p xs'
 
@@ -225,46 +196,12 @@ unwords ws       =  foldr1 (\w s -> w ++ ' ':s) ws
 reverse          :: [a] -> [a]
 reverse          =  foldl (flip (:)) []
 
--- and returns the conjunction of a Boolean list.  For the result to be
--- True, the list must be finite; False, however, results from a False
--- value at a finite index of a finite or infinite list.  or is the
--- disjunctive dual of and.
-and, or          :: [Bool] -> Bool
-and              =  foldr (&&) True
-or               =  foldr (||) False
-
--- Applied to a predicate and a list, any determines if any element
--- of the list satisfies the predicate.  Similarly, for all.
-any, all         :: (a -> Bool) -> [a] -> Bool
-any p            =  or . map p
-all p            =  and . map p
-
--- elem is the list membership predicate, usually written in infix form,
--- e.g., x `elem` xs.  notElem is the negation.
-elem, notElem    :: (Eq a) => a -> [a] -> Bool
-elem x           =  any (== x)
-notElem x        =  all (/= x)
-
 -- lookup key assocs looks up a key in an association list.
 lookup           :: (Eq a) => a -> [(a,b)] -> Maybe b
 lookup key []    =  Nothing
 lookup key ((x,y):xys)
     | key == x   =  Just y
     | otherwise  =  lookup key xys
-
--- sum and product compute the sum or product of a finite list of numbers.
-sum, product     :: (Num a) => [a] -> a
-sum              =  foldl (+) 0  
-product          =  foldl (*) 1
-
--- maximum and minimum return the maximum or minimum value from a list,
--- which must be non-empty, finite, and of an ordered type.
-maximum, minimum :: (Ord a) => [a] -> a
-maximum []       =  error "Prelude.maximum: empty list"
-maximum xs       =  foldl1 max xs
-
-minimum []       =  error "Prelude.minimum: empty list"
-minimum xs       =  foldl1 min xs
 
 -- zip takes two lists and returns a list of corresponding pairs.  If one
 -- input list is short, excess elements of the longer list are discarded.
@@ -293,7 +230,7 @@ zipWith3 z (a:as) (b:bs) (c:cs)
 zipWith3 _ _ _ _ =  []
 
 
--- unzip transforms a list of pairs into a pair of lists.  
+-- unzip transforms a list of pairs into a pair of lists.
 
 unzip            :: [(a,b)] -> ([a],[b])
 unzip            =  foldr (\(a,b) ~(as,bs) -> (a:as,b:bs)) ([],[])
@@ -301,5 +238,3 @@ unzip            =  foldr (\(a,b) ~(as,bs) -> (a:as,b:bs)) ([],[])
 unzip3           :: [(a,b,c)] -> ([a],[b],[c])
 unzip3           =  foldr (\(a,b,c) ~(as,bs,cs) -> (a:as,b:bs,c:cs))
                           ([],[],[])
-
-
